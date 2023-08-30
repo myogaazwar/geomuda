d70e3b878eaf355066c8ac11e5e4e9d7405e9ddc
Alvitto Aprilian Gumay <alvittogenz09@gmail.com>
2023-08-30 14:06:06 +0700

    add : navbar scroll remove transapernt

--- app/page.jsx
+++ app/page.jsx
@@ -10,6 +10,117 @@ const LandingPage = () => {
 
         <Banner />
       </div>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        nihil delectus autem maiores asperiores molestiae, sequi voluptate quasi
+        ducimus hic cupiditate quibusdam in culpa cumque consequatur aliquid
+        soluta incidunt tempora deserunt ab tempore nostrum. Possimus fuga
+        molestias dolore explicabo esse commodi tempora corrupti quo rem. Illo
+        odit repudiandae, in dolorem ipsum impedit consequatur aspernatur
+        officia asperiores, perspiciatis temporibus? Eius, in! Hic nisi nam
+        dignissimos fugiat modi animi vitae porro, libero adipisci excepturi rem
+        non unde omnis at! Earum tempore dignissimos quia culpa atque quae
+        necessitatibus ea quis ex odio, dicta, nihil maiores similique delectus,
+        incidunt architecto! Laboriosam quidem asperiores nemo doloremque quia
+        dicta magni eos illum, voluptatem quaerat error fugiat eius sed,
+        voluptates culpa libero eligendi atque saepe maxime soluta
+        exercitationem minus harum hic! Fugit ut optio sapiente architecto ullam
+        laudantium iure obcaecati minima eius ipsa. Quidem unde quaerat deleniti
+        quia ipsum quam ab quod non laboriosam esse! Accusamus quia harum
+        recusandae cumque sequi aliquid ex. Ipsam atque iste temporibus
+        doloribus? Nulla doloribus dolorum soluta, aliquam quibusdam vitae non?
+        Excepturi amet et aperiam in error iusto similique, aliquid tempora esse
+        libero voluptatibus repudiandae nobis. Numquam quibusdam reiciendis
+        quaerat alias voluptates molestiae eos asperiores similique, iusto
+        ratione! Qui doloremque doloribus recusandae non ullam earum dicta
+        accusamus cumque expedita tempora quo repellendus optio consequuntur
+        autem temporibus quod sapiente at velit, obcaecati dolores eum ad minus
+        soluta! Ea est explicabo in, natus cum aut eos distinctio ut amet
+        perspiciatis cupiditate impedit ipsum eius eaque fugiat mollitia maxime
+        sequi aliquid ratione earum officiis dolor. Cupiditate, facilis. Modi
+        laboriosam, nemo aliquam perspiciatis illo harum esse sapiente atque
+        exercitationem et nobis aut tenetur iste nisi officia illum quae eius
+        dolor expedita qui amet! Pariatur illum a provident, optio cupiditate
+        dolorum porro sint ex sed praesentium delectus voluptates omnis error,
+        culpa debitis ut. Placeat ut eveniet tenetur. Asperiores quae deserunt
+        beatae minus reiciendis assumenda esse tenetur laboriosam ut minima,
+        sapiente deleniti hic blanditiis maiores quam dolorum. Accusamus ex quae
+        unde voluptas asperiores maxime ut totam blanditiis, minima eius
+        delectus accusantium libero harum culpa quisquam eos vel quod quas
+        fugiat laudantium quaerat nisi repellendus? Ab ullam hic explicabo
+        placeat vero. Rerum fuga vitae, laborum a quae obcaecati voluptatibus
+        quaerat voluptatum voluptate ducimus sapiente sit similique, suscipit
+        iste odit magnam sint voluptates distinctio, adipisci dolores
+        necessitatibus. Aut, repellendus, veritatis laboriosam nulla hic
+        perspiciatis, minima voluptatibus autem odit quaerat vero nesciunt
+        libero amet ex iusto odio laborum dolore eius neque vel. Repellendus
+        ullam quae, possimus et exercitationem doloribus sint dolorem fugiat
+        dolor facere odio nulla dolores sit nihil vel in provident neque minus
+        eaque quod aliquam officiis voluptas labore veritatis! Doloremque
+        accusantium rem reprehenderit dolor quisquam eum, fugit quod error
+        tenetur aperiam atque quam voluptas cum voluptates temporibus blanditiis
+        quae mollitia a harum possimus, sunt itaque, commodi necessitatibus!
+        Cupiditate omnis tenetur, consequatur laudantium fugiat porro illo
+        cumque sint neque quis similique magnam eum quo labore soluta? Omnis
+        expedita vitae earum, molestiae recusandae dignissimos officiis tempore
+        nisi id ab. Laborum eum ipsa sed corporis adipisci dignissimos beatae
+        possimus, omnis enim. Officiis vel tempore ad quo esse corporis, quas
+        harum rem numquam distinctio, ex nulla impedit eveniet explicabo
+        voluptatem similique adipisci voluptas quasi nam cum? Eos, voluptas!
+        Obcaecati et, quas quidem sint tenetur, natus expedita provident sed
+        libero aliquam quam accusamus iure dignissimos excepturi in cupiditate
+        ipsa odit vel perspiciatis nihil eius? Corrupti explicabo earum,
+        aspernatur eligendi facilis perferendis ut accusantium aperiam eaque qui
+        quo, laudantium tempore voluptates deleniti deserunt incidunt,
+        consequatur itaque iure consequuntur quia nihil dolore error minima
+        debitis. Quasi libero sunt sit repellat iure hic ullam recusandae harum
+        temporibus dolor, aut ea delectus, quo ex et omnis, eius nam sapiente.
+        Distinctio ut non quisquam rem nobis fugiat tempore nihil omnis est!
+        Tempore deleniti earum, quae laborum tenetur pariatur voluptatibus
+        repellendus possimus mollitia, rerum odio. Minima aliquid sapiente
+        consectetur dolor ab, laudantium, quod facilis totam aut aliquam est
+        asperiores eum cupiditate. Beatae sit repudiandae magni sapiente aperiam
+        tenetur quisquam sint facere iure facilis, debitis soluta dolor ratione
+        recusandae numquam, commodi officia suscipit maiores temporibus
+        reprehenderit a. Possimus tempora adipisci vitae, voluptatem corrupti
+        quod voluptates maiores ipsa maxime illo accusamus fuga est? Veritatis
+        quae eos at minus neque blanditiis eum dignissimos aliquid repellendus
+        pariatur laboriosam corporis dolorum impedit tempore dolores, ratione
+        quis, suscipit aperiam modi. Provident, a. Omnis nesciunt consequatur
+        earum repellat doloribus dolorum quam, quas odio! Vel architecto fugit
+        quia praesentium quae nemo, in unde soluta reprehenderit quisquam magni
+        nisi labore laudantium quam id consequatur dolores dolore qui omnis
+        reiciendis quasi. Dolorum laudantium, voluptates tempora doloribus
+        minima voluptatum nisi minus rerum, nemo dolore maxime ratione fuga amet
+        exercitationem enim reiciendis ipsa consectetur temporibus, vitae illum
+        ea non pariatur necessitatibus officia. Laboriosam sapiente nihil, ullam
+        cumque quibusdam itaque aliquid consectetur, eos eius mollitia cum
+        aperiam veritatis excepturi ut error eum illo? Sed ea quaerat, pariatur
+        quasi dicta obcaecati? Soluta maxime sapiente minima libero ab
+        provident! Sequi molestias magnam ratione ullam deserunt, nulla quisquam
+        voluptate laudantium fuga dolores nam earum distinctio similique error
+        vitae tempore reiciendis ipsam reprehenderit, possimus excepturi et iure
+        est nobis! Qui culpa cupiditate officia odio ea, consectetur aspernatur
+        alias laboriosam libero sunt aliquam, ullam voluptates dolorum obcaecati
+        est officiis debitis nihil accusantium! Sapiente, quibusdam dolore error
+        iusto distinctio consequatur qui provident, similique temporibus, sed
+        quo libero magni. Veniam, amet itaque nihil ipsam esse suscipit
+        repudiandae cumque repellat fuga necessitatibus rem sed, voluptatem odit
+        iure, non laudantium nobis? Maiores, accusamus voluptates? Voluptate
+        corporis illum est minima unde rem eveniet distinctio neque nostrum,
+        maxime sed quas possimus. Assumenda autem cupiditate id quas deleniti
+        modi, blanditiis perspiciatis accusamus quae aperiam suscipit at,
+        molestias rerum dolore pariatur officiis veritatis? Aliquam sint quia
+        neque, ducimus eaque sit consectetur doloribus quas nostrum praesentium
+        maxime obcaecati, alias deleniti ratione quo eum qui nisi quam commodi
+        expedita eligendi illum consequatur. Quam, quos rem illo eos fugit,
+        laudantium nemo unde sapiente optio ipsum esse distinctio consequatur
+        sunt at et ab molestiae cum perferendis impedit repellat beatae
+        recusandae libero assumenda facere? Nostrum saepe alias, vitae voluptate
+        natus vel, consectetur illo fuga non quia delectus, reprehenderit
+        consequatur voluptas aliquam!
+      </p>
     </main>
   );
 };
--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,14 +1,40 @@
+"use client";
+
 // ** Import Assets
 import { navItems } from "@/constanst/navItems";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [scroll, setScroll] = useState(0);
+
+  const handleScroll = () => {
+    setScroll(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="flex fixed top-0 w-full text-white items-center bg-transparent justify-between py-10 px-32">
+    <nav
+      className={`flex fixed top-0 w-full text-white items-center justify-between py-8 px-32 ${
+        scroll > 0
+          ? "bg-green-600 z-50 transition duration-700"
+          : "bg-transparent"
+      }`}
+    >
       <h1 className="text-3xl font-bold cursor-not-allowed">Logo</h1>
 
       <div className="flex gap-5">
-        {navItems.map((item) => (
-          <div className="border cursor-pointer border-white px-4 py-2 rounded-xl">
+        {navItems.map((item, index) => (
+          <div
+            key={index}
+            className="border cursor-pointer border-white px-4 py-2 rounded-xl"
+          >
             {item.name}
           </div>
         ))}
